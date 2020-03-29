e58626e705003e6181dfbea982911ac1dc10b965
Mark Poepping <mark.poepping@mmpro.de>
2020-03-29 16:27:46 +0200

    fix(App): Prepare repository for AC semantic release | MP
    Cleaned up repository and use ac-semantic-release

---
+++ .acsemver.js
@@ -0,0 +1,9 @@
+module.exports = {
+  repository: {
+    url: 'https://github.com/mmpro/ac-countrylist'
+  },
+  changelogFile: __dirname + '/CHANGELOG.md',
+  sections: [
+    {name: 'App' }
+  ],
+}
