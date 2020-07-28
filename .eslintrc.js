998661efecaf96de93f1e081446d3cef508b4e13
Mark Poepping <mark.poepping@mmpro.de>
2020-07-28 13:37:57 +0200

    chore(App): Updated ESlint | MP
    Updated ESlint instructions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
-const config = {
-  "extends": "standard",
-  "plugins": [
-    "standard",
-    "promise",
-  ],
+let config = {
+  root: true,
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
   "rules": {
     "space-before-function-paren": 0,
     "no-extra-semi": 0,
@@ -11,10 +13,13 @@ const config = {
     "brace-style": ["error", "stroustrup", { "allowSingleLine": true }],
     "no-useless-escape": 0,
     "standard/no-callback-literal": 0,
-    "new-cap": 0
+    "new-cap": 0,
+    "no-console": ["error", { allow: ["error"] }]
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018
   },
   globals: {
-    config: true,
     describe: true,
     it: true
   }
